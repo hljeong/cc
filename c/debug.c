de37da1a9b834989a86e77374bd134ce638ae2ac
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2026-03-24 22:58:18 -0700

    debugf_span

--- c/debug.c
+++ c/debug.c
@@ -18,6 +18,18 @@ static void vdebugf_at(const char *loc, const char *fmt, va_list ap) {
   debugf("%*s^ ", col, ""); vdebugf(fmt, ap); debugf("\n");
 }
 
+static void vdebugf_span(const StringView span, const char *fmt, va_list ap) {
+  const int col = span.loc - ctx.src;
+  assertf(0 <= col && col + span.len <= ctx.src_len,
+          "invalid span: (%d, %d), src_len=%d",
+          col, span.len, ctx.src_len);
+
+  debugf("%s\n", ctx.src);
+  debugf("%*s", col, "");
+  for (int i = 0; i < span.len; i++) debugf("%c", '~');
+  debugf(" "); vdebugf(fmt, ap); debugf("\n");
+}
+
 void debugf_loc(const char *fmt, ...) {
   va_list ap; va_start(ap, fmt);
   vdebugf_at(ctx.lexer.loc, fmt, ap);
@@ -25,7 +37,7 @@ void debugf_loc(const char *fmt, ...) {
 
 void debugf_tok(const char *fmt, ...) {
   va_list ap; va_start(ap, fmt);
-  vdebugf_at(ctx.parser.tok->lexeme.loc, fmt, ap);
+  vdebugf_span(ctx.parser.tok->lexeme, fmt, ap);
 }
 
 void _assert(const char *file, const int line, const char *cond) {
@@ -75,6 +87,6 @@ void errorf_loc(const char *fmt, ...) {
 
 void errorf_tok(const char *fmt, ...) {
   va_list ap; va_start(ap, fmt);
-  vdebugf_at(ctx.parser.tok->lexeme.loc, fmt, ap);
+  vdebugf_span(ctx.parser.tok->lexeme, fmt, ap);
   exit(1);
 }
